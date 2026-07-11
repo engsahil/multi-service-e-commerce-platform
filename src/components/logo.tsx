8a6782a10b3de3bd2e9d02bc63f11e72b2e134ee
sahil <engsahil13@gmail.com>
2026-07-10 21:03:46 -0700

    Change logo to Servixa

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo({ compact = false }: { compact?: boolean }) {
   return (
     <Link href="/" className="logo" aria-label="Servixa home">
       <span className="logo-mark" aria-hidden="true">S</span>
-      {!compact && <span>Mr. Sahil <b>IT</b></span>}
+      {!compact && <span>Servi<b>xa</b></span>}
     </Link>
   );
 }
