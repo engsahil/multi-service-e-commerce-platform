431efa0ef72bba1d5177f1378264154dd462d981
sahil <engsahil13@gmail.com>
2026-07-10 22:00:31 -0700

    Added Servixa logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,28 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Logo({ compact = false }: { compact?: boolean }) {
   return (
     <Link href="/" className="logo" aria-label="Servixa home">
       <Image
-        src="/servixa-logo.png"
+        src="/logo.png"
         alt="Servixa"
-        width={170}
-        height={60}
+        width={40}
+        height={40}
         priority
       />
+
+      {!compact && (
+        <span
+          style={{
+            fontWeight: 700,
+            fontSize: "1.25rem",
+            marginLeft: "10px",
+          }}
+        >
+          Servixa
+        </span>
+      )}
     </Link>
   );
 }
